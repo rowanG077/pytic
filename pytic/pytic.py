0e3de9f1781987c3b9243a430c6cb9396bf22abe
Henry Von Tresckow <hvontresckow@kns.com>
2020-01-17 15:14:54 -0800

    Add Support for loading 32bit drivers if needed
    - Change from ctypes.windll to ctypes.cdll import to get pointer sizes to be
    corredt for 32 bit python on 64 bit windows.

--- pytic/pytic.py
+++ pytic/pytic.py
@@ -7,6 +7,7 @@ from .pytic_protocol import tic_constant as tc
 from .pytic_structures import *
 from functools import wraps, partial
 import logging
+import platform
 
 # [T]ic [E]rror [D]ecoder
 def TED(func):
@@ -77,8 +78,12 @@ class PyTic(object):
         # Driver Locations (x64)
         file_path = os.path.dirname(os.path.abspath(__file__))
         #file_path = file_path[:-len('pytic')]
-        self.usblib = windll.LoadLibrary(file_path+"\\drivers\\x64\\libusbp-1.dll")
-        self.ticlib = windll.LoadLibrary(file_path+"\\drivers\\x64\\libpololu-tic-1.dll")
+		if platform.architecture()[0] == '32bit':
+			self.usblib = cdll.LoadLibrary(file_path+"\\drivers\\Win32\\libusbp-1.dll")
+			self.ticlib = cdll.LoadLibrary(file_path+"\\drivers\\Win32\\libpololu-tic-1.dll")
+		else:
+			self.usblib = cdll.LoadLibrary(file_path+"\\drivers\\Win64\\libusbp-1.dll")
+			self.ticlib = cdll.LoadLibrary(file_path+"\\drivers\\Win64\\libpololu-tic-1.dll")
 
     def _create_tic_command_attributes(self):
         for c in self._commands:
